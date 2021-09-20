24f1ed1825457fe6ce4718a52d9bebe414cf7f92
BassmatiOnMusic <bassmatiOnMusic@freenet.de>
2021-09-20 16:35:44 +0200

    initial commit

---
+++ start-msedge.bat
@@ -0,0 +1,1 @@
+"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" "http://localhost:4507/circular-scale.htm?collapse=none"
---
+++ start-vs.bat
@@ -0,0 +1,1 @@
+c:\progs\VS2022\Common7\IDE\devenv.com /command File.OpenFolder "D:\github\music-toolbox"
